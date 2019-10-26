94bba3bd1604ab39ca01b7d4c527074cd26da9d0
Samdaaman <samuel.m.hogan@gmail.com>
2019-10-26 16:21:03 +1300

    beta still

--- megatron.py
+++ megatron.py
@@ -7,6 +7,10 @@ PORT = '7890'
 def main():
     print('Initialising Communications on port:' + PORT)
     process = subprocess.Popen(["nc", "-lvp", PORT], stdout=subprocess.PIPE, universal_newlines=True)
+    while process.stdout.readline() != 'Listening on [0.0.0.0] (family 0, port ' + PORT + ')'
+        if process.poll() is not None:
+            raise Exception('Unexpected Error 101')
+    print('Connection from Host received')
     while True:
         line = process.stdout.readline()
         if line == '' and process.poll() is not None:
